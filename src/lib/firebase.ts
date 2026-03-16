9bc550d776f1f114bce3daffb2fbe7c5629b91b9
Zyrus C. Velasco <zyvel0@gmail.com>
2026-03-16 15:26:41 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
 
 // This file is deprecated. Use standardized hooks from '@/firebase' instead.
-// For initialization, see src/firebase/index.ts
+// For initialization, see src/firebase/index.t
 export {};
